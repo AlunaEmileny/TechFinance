e96d04913e9dbc9dbdf6a61a22182dbc281a807c
AlunaEmileny <capic.2023118tads0008@aluno.ifpi.edu.br>
2023-09-06 19:43:43 -0300

    Operaçẽs realizadas

--- src/Banco.java
+++ src/Banco.java
@@ -2,14 +2,22 @@ import java.util.Scanner;;
 public class Banco {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Olá, digite seu nome: ");
         String nome = scanner.next();
+        System.out.println("Olá, digite seu CPF: ");       
         String cpf = scanner.next();
+        System.out.println("Olá, digite seu email: ");
         String email = scanner.next();
         Cliente c1 = new Cliente(nome, cpf, email);
         c1.imprimir();
-        Conta conta = new Conta("000000-9", "c1", "000-1", 0);
 
+        // Teste da class Conta
+        Conta conta = new Conta("1234321-1", "0001", 900);
+        conta.sacar(230);
+        conta.consultarExtrato();
+        conta.sacar(1000);
         scanner.close();
+
     }
 
 }
