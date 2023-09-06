b741f3a78b9b26b9dd8f0092d074cc555da21c48
AlunaEmileny <capic.2023118tads0008@aluno.ifpi.edu.br>
2023-09-05 21:07:56 -0300

    Estrutura do projeto

---
+++ src/Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente {
+    
+    String nome, cpf, email;
+    public Cliente(String nome, String cpf, String email){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+    public void imprimir(){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("email: " + this.email);
+        System.out.println("cpf: " + this.cpf);
+    }
+}
